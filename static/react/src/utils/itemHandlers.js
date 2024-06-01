54529631f8f8ce1d8823f57bc6d890b87ba48272
Anthony Donnelly <amzo1337@gmail.com>
2024-06-01 06:24:43 +0100

    Fix workspace, and added icons for the toolbox. Workspace can now be dragged and dropped freely

--- static/react/src/utils/itemHandlers.js
+++
@@ -1,8 +0,0 @@
-export const handleItemClick = (item, setSelectedItem) => {
-    setSelectedItem(item);
-};
-
-export const handleDelete = (item, workspaceItems, setWorkspaceItems, setSelectedItem) => {
-    setWorkspaceItems(workspaceItems.filter(workspaceItem => workspaceItem.id !== item.id));
-    setSelectedItem(null);
-};
